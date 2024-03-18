2881a7a8673fe7bf85d627effc6f48738b1e7c5f
O-Babasola <160128209+O-Babasola@users.noreply.github.com>
2024-03-18 22:50:13 +0000

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,15 @@
+#numbers.py code is practical task 2
+num_1 = int(input("Enter a first number: "))
+num_2 = int(input("Enter a second number: "))
+num_3 = int(input("Enter a third number: "))
+sum_of_nums = num_1 + num_2 + num_3
+print(sum_of_nums)
+
+minus = num_1 - num_2
+print(minus)
+
+multiply = num_3*num_1
+print(multiply)
+
+divided = sum_of_nums/num_3
+print(divided)
