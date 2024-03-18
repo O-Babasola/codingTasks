2c5584c2007b75410f638205e6d6efce5eb88dcb
O-Babasola <160128209+O-Babasola@users.noreply.github.com>
2024-03-18 22:51:35 +0000

    Add files via upload

---
+++ conversion.py
@@ -0,0 +1,25 @@
+#Practical Task 4
+
+
+#declare num1 and store 99.23 as its variable
+#declare num2 and store 23 as its variable
+#declare num3 and store 150 as its variable
+#declare string1 and store "100" as its variable
+
+#convert num1 into an integer
+#convert num2 into a float (nummber with a decimal)
+#convert num3 into a string
+#convert string1 into an integer
+
+#output the new variables created on separate lines
+
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = "100"
+
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
